a98e31c66279920c60b2609657d3e65a58d81ef3
juniorjose1 <jajrjunior95@gmail.com>
2021-10-01 20:28:17 -0300

    v1 - Criação dos componentes e dos visuais dos mesmos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+import { ExtratoComponent } from './extrato/extrato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovaTransferenciaComponent,
+    ExtratoComponent
   ],
   imports: [
     BrowserModule,
